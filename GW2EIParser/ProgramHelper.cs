28027027f2b4a9f282f7b86cf4f8ee739271f6e6
lother <lotherex@gmail.com>
2022-12-03 23:59:09 +0800

    update

--- GW2EIParser/ProgramHelper.cs
+++ GW2EIParser/ProgramHelper.cs
@@ -55,19 +55,192 @@ namespace GW2EIParser
             return builder;
         }
 
-        private static Embed BuildEmbed(ParsedEvtcLog log, string dpsReportPermalink)
+        private static Embed BuildEmbedLother(ParsedEvtcLog log, string dpsReportPermalink)
         {
             EmbedBuilder builder = GetEmbedBuilder();
-            builder.WithThumbnailUrl(log.FightData.Logic.Icon);
             //
-            builder.AddField("Encounter Duration", log.FightData.DurationString);
-            //
-            if (log.FightData.Logic.GetInstanceBuffs(log).Any())
+            builder.AddField("Encounter Duration", log.FightData.DurationString, true);
+            builder.AddField("Recorded By", log.LogData.PoVName, true);
+            var players = new List<AbstractSingleActor>(log.PlayerList.Where(x => !x.IsFakeActor));
+            var target = new List<AbstractSingleActor>(log.FightData.Logic.Targets.Where(x => x.Character.Length > 0 && typeof(PlayerNonSquad) == x.GetType()));
+            IReadOnlyList<PhaseData> phases = log.FightData.GetPhases(log);
+            string s = "";
+            int totalDamage = 0;
+            long totalDamageTaken = 0;
+            int totalBarrier = 0;
+            int totalDps = 0;
+            int totalCondiCleanse = 0;
+            int totalBoonStrips = 0;
+            int totalDowns = 0;
+            int totalDowneds = 0;
+            int totalKill = 0;
+            int totalDeaths = 0;
+            var boonStrips = new List<KeyValuePair<string, int>>();
+            var condiCleanse = new List<KeyValuePair<string, int>>();
+            var dps = new List<KeyValuePair<string, (int, int)>>();
+            var tag = new List<string>();
+            var prof = new Dictionary<ParserHelper.Spec, string>();
+
+
+
+            prof.Add(ParserHelper.Spec.Necromancer, "Nec");
+            prof.Add(ParserHelper.Spec.Reaper, "Rea");
+            prof.Add(ParserHelper.Spec.Scourge, "Scg");
+            prof.Add(ParserHelper.Spec.Harbinger, "Har");
+
+            prof.Add(ParserHelper.Spec.Elementalist, "Ele");
+            prof.Add(ParserHelper.Spec.Tempest, "Tmp");
+            prof.Add(ParserHelper.Spec.Weaver, "Wea");
+            prof.Add(ParserHelper.Spec.Catalyst, "Cat");
+
+            prof.Add(ParserHelper.Spec.Mesmer, "Mes");
+            prof.Add(ParserHelper.Spec.Chronomancer, "Chr");
+            prof.Add(ParserHelper.Spec.Mirage, "Mir");
+            prof.Add(ParserHelper.Spec.Virtuoso, "Vir");
+
+            prof.Add(ParserHelper.Spec.Warrior, "War");
+            prof.Add(ParserHelper.Spec.Berserker, "Brs");
+            prof.Add(ParserHelper.Spec.Spellbreaker, "Spb");
+            prof.Add(ParserHelper.Spec.Bladesworn, "Bds");
+
+            prof.Add(ParserHelper.Spec.Revenant, "Rev");
+            prof.Add(ParserHelper.Spec.Herald, "Her");
+            prof.Add(ParserHelper.Spec.Renegade, "Ren");
+            prof.Add(ParserHelper.Spec.Vindicator, "Vin");
+
+            prof.Add(ParserHelper.Spec.Guardian, "Gdn");
+            prof.Add(ParserHelper.Spec.Dragonhunter, "Dgh");
+            prof.Add(ParserHelper.Spec.Firebrand, "Fbd");
+            prof.Add(ParserHelper.Spec.Willbender, "Wbd");
+
+            prof.Add(ParserHelper.Spec.Thief, "Thf");
+            prof.Add(ParserHelper.Spec.Daredevil, "Dar");
+            prof.Add(ParserHelper.Spec.Deadeye, "Ded");
+            prof.Add(ParserHelper.Spec.Specter, "Spe");
+
+            prof.Add(ParserHelper.Spec.Ranger, "Rgr");
+            prof.Add(ParserHelper.Spec.Druid, "Dru");
+            prof.Add(ParserHelper.Spec.Soulbeast, "Slb");
+            prof.Add(ParserHelper.Spec.Untamed, "Unt");
+
+            prof.Add(ParserHelper.Spec.Engineer, "Eng");
+            prof.Add(ParserHelper.Spec.Scrapper, "Scr");
+            prof.Add(ParserHelper.Spec.Holosmith, "Hls");
+            prof.Add(ParserHelper.Spec.Mechanist, "Mec");
+
+
+            prof.Add(ParserHelper.Spec.NPC, "NPC");
+            prof.Add(ParserHelper.Spec.Gadget, "Gadget");
+            prof.Add(ParserHelper.Spec.Unknown, "Unknown");
+
+            foreach (Player player in players)
             {
-                builder.AddField("Instance Buffs", string.Join("\n", log.FightData.Logic.GetInstanceBuffs(log).Select(x => (x.stack > 1 ? x.stack + " " : "") + x.buff.Name)));
+                string name;
+                if (prof.ContainsKey(player.Spec))
+                {
+                    name = $"{player.Character} ({prof[player.Spec]})";
+                }
+                else
+                {
+                    name = $"{player.Character} ({player.Spec})";
+                }
+                if (player.IsCommander(log))
+                {
+                    tag.Add($"{player.Character} ({player.Account})");
+                }
+                int playerDamage = 0;
+                int playerDps = 0;
+                foreach (PlayerNonSquad n in target)
+                {
+                    FinalDPS adps = player.GetDPSStats(n, log, phases[0].Start, phases[0].End);
+                    playerDamage += adps.Damage;
+                    playerDps += adps.Dps;
+                    FinalOffensiveStats gps = player.GetOffensiveStats(n, log, phases[0].Start, phases[0].End);
+
+                    totalKill += gps.Killed;
+                    totalDowneds += gps.Downed;
+                }
+                dps.Add(new KeyValuePair<string, (int, int)>(name, (playerDamage, playerDps)));
+                totalDamage += playerDamage;
+                totalDps += playerDps;
+
+                FinalDefensesAll def = player.GetDefenseStats(log, phases[0].Start, phases[0].End);
+                totalDowns += def.DownCount;
+                totalDeaths += def.DeadCount;
+                totalDamageTaken += def.DamageTaken;
+                totalBarrier += def.DamageBarrier;
+
+                FinalToPlayersSupport sp = player.GetToPlayerSupportStats(log, phases[0].Start, phases[0].End);
+                condiCleanse.Add(new KeyValuePair<string, int>(name, sp.CondiCleanse));
+                boonStrips.Add(new KeyValuePair<string, int>(name, sp.BoonStrips));
+                totalCondiCleanse += sp.CondiCleanse;
+                totalBoonStrips += sp.BoonStrips;
             }
-            //
-            /*var playerByGroup = log.PlayerList.Where(x => !x.IsFakeActor).GroupBy(x => x.Group).ToDictionary(x => x.Key, x => x.ToList());
+            int length = (dps.Count >= 10) ? 10 : dps.Count;
+            dps = dps.OrderByDescending(x => x.Value.Item1).ToList().GetRange(0, length);
+            condiCleanse = condiCleanse.OrderByDescending(x => x.Value).ToList().GetRange(0, length);
+            boonStrips = boonStrips.OrderByDescending(x => x.Value).ToList().GetRange(0, length);
+            if (tag.Count > 0)
+            {
+                builder.AddField("Commander", string.Join("\n", tag));
+            }
+            string kd = "∞";
+            if (totalDeaths > 0)
+            {
+                kd = ((float)totalKill / totalDeaths).ToString("F2");
+            }
+            builder.AddField("Squad Summary", "```CSS\n" +
+               $" Player      Damage      DPS     Downs   Deaths\n" +
+               $"--------  -----------  -------  -------  -------\n" +
+               $"   {players.Count,-2}     {totalDamage.ToString("N0"),11}  {totalDps.ToString("N0"),7}     {totalDowns,-2}       {totalDeaths,-2}\n" +
+               $"DamageTaken    Barrier    Cleanses  Strips\n" +
+               $"-----------  -----------  --------  ------\n" +
+               $"{totalDamageTaken.ToString("N0"),11}  {totalBarrier.ToString("N0"),11}    {totalCondiCleanse,-4}     {totalBoonStrips,-4}\n" +
+               $" Enemy    Downeds   Kill    K/D\n" +
+               $"--------  -------  ------  -----\n" +
+               $"   {target.Count,-3}        {totalDowneds,-3}      {totalKill,-3}  {kd}\n" +
+                "```");
+            string dpsString = "";
+            string condiCleanseString = "";
+            string boonStripsString = "";
+            int c;
+            c = 1;
+            foreach (KeyValuePair<string, (int, int)> d in dps)
+            {
+                dpsString += $"{c,2}.  {d.Key,25}  {d.Value.Item1.ToString("N0"),9}  {d.Value.Item2.ToString("N0"),7}\n";
+                c++;
+            }
+            c = 1;
+            foreach (KeyValuePair<string, int> cc in condiCleanse)
+            {
+                condiCleanseString += $"{c,2}.  {cc.Key,25}  {cc.Value,6}\n";
+                c++;
+            }
+            c = 1;
+            foreach (KeyValuePair<string, int> bs in boonStrips)
+            {
+                boonStripsString += $"{c,2}.  {bs.Key,25}  {bs.Value,5}\n";
+                c++;
+            }
+
+            builder.AddField("Damage Summary", "```CSS\n" +
+               $" #  Player                       Damage      DPS  \n" +
+               $"--- --------------------------  ---------  -------\n" +
+                dpsString +
+                "```");
+            builder.AddField("Cleanse Summary", "```CSS\n" +
+               $" #  Player                       Cleanses\n" +
+               $"--- --------------------------  ----------\n" +
+                condiCleanseString +
+                "```");
+            builder.AddField("Strips Summary", "```CSS\n" +
+               $" #  Player                       Strips\n" +
+               $"--- --------------------------  --------\n" +
+                boonStripsString +
+                "```");
+
+            /*
+            var playerByGroup = log.PlayerList.Where(x => !x.IsFakeActor).GroupBy(x => x.Group).ToDictionary(x => x.Key, x => x.ToList());
             var hasGroups = playerByGroup.Count > 1;
             foreach (KeyValuePair<int, List<Player>> pair in playerByGroup)
             {
@@ -77,20 +250,25 @@ namespace GW2EIParser
                     groupField.Add(p.Character + " - " + p.Prof);
                 }
                 builder.AddField(hasGroups ? "Group " + pair.Key : "Party Composition", String.Join("\n", groupField));
-            }*/
+            }
+            //*/
             //
-            builder.AddField("Game Data", "ARC: " + log.LogData.ArcVersion + " | " + "GW2 Build: " + log.LogData.GW2Build);
-            //
-            builder.WithTitle(log.FightData.FightName);
             //builder.WithTimestamp(DateTime.Now);
+
             builder.WithFooter(log.LogData.LogStartStd + " / " + log.LogData.LogEndStd);
             builder.WithColor(log.FightData.Success ? Color.Green : Color.Red);
-            if (dpsReportPermalink.Length > 0)
+            if (dpsReportPermalink.Length > 0 && dpsReportPermalink != "Upload process failed")
             {
                 builder.WithUrl(dpsReportPermalink);
+                builder.WithTitle(log.FightData.FightName);
+            }
+            else
+            {
+                builder.WithTitle(log.FightData.FightName + $"({dpsReportPermalink})");
             }
             return builder.Build();
         }
+
 
         private static bool HasFormat()
         {
@@ -175,7 +353,7 @@ namespace GW2EIParser
                     } 
                     else
                     {
-                        WebhookController.SendMessage(Properties.Settings.Default.WebhookURL, BuildEmbed(log, uploadStrings[0]),out string message);
+                        WebhookController.SendMessage(Properties.Settings.Default.WebhookURL, BuildEmbedLother(log, uploadStrings[0]),out string message);
                         operation.UpdateProgressWithCancellationCheck(message);
                     }
                 }
